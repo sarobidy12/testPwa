0498fa0f94609df56d5a0ffd4cc99e36913799c0
bidy <sarobidyalliance@outlook.com>
2021-12-10 19:33:12 +0300

    verify

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import './App.css';
 
 class App extends Component {
 
+  componentDidMount() {
+    window.addEventListener('appinstalled', (evt) => {
+      console.log('a2hs installed');
+    })
+  }
+
   render() {
     return (
       <h1>Hello word</h1>
